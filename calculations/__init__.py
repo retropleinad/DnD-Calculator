60c1fbc18ebf627dfdf4b65302cb3b1f848baf85
Daniel Porter <73040840+retropleinad@users.noreply.github.com>
2021-04-05 19:50:37 -0500

    Add files via upload

---
+++ calculations/__init__.py
@@ -0,0 +1,8 @@
+"""
+This package contains files for making calculations in the app's backend.
+
+regressions.py holds files for creating and using linear regressions to calculate CR
+
+damage.py contains classes for handling calculations over attacks
+
+"""

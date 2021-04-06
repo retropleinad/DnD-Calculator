b580093e24ac23445aa757aa915c17c5eabf39d2
Daniel Porter <73040840+retropleinad@users.noreply.github.com>
2021-04-05 20:15:20 -0500

    Add files via upload

--- gui/monsters.py
+++ gui/monsters.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 
+from gui import help
+
 
 # Class for the monster lookup screen GUI
 class MonsterLookupScreen:
@@ -36,6 +38,7 @@ class MonsterLookupScreen:
         self.languages_entry = None
 
         self.search_button = None
+        self.help_button = None
 
         self.draw_labels()
         self.draw_entries()
@@ -56,6 +59,16 @@ class MonsterLookupScreen:
             label = tk.Label(master=self.screen, text=self.attributes[i], width=15, height=1)
             self.attribute_labels.append(label)
 
+        self.name_label.grid(row=0, column=0, padx=1, pady=5)
+        self.hp_label.grid(row=1, column=0, padx=1, pady=5)
+        self.speed_label.grid(row=2, column=0, padx=1, pady=5)
+        self.cr_label.grid(row=3, column=0, padx=1, pady=5)
+        self.type_label.grid(row=4, column=0, padx=1, pady=5)
+        self.size_label.grid(row=5, column=0, padx=1, pady=5)
+        self.alignment_label.grid(row=6, column=0, padx=1, pady=5)
+        self.senses_label.grid(row=7, column=0, padx=1, pady=5)
+        self.languages_label.grid(row=8, column=0, padx=1, pady=5)
+
     def draw_entries(self):
         self.name_entry = tk.Entry(master=self.screen, width=10)
         self.hp_entry = tk.Entry(master=self.screen, width=10)
@@ -67,5 +80,25 @@ class MonsterLookupScreen:
         self.senses_entry = tk.Entry(master=self.screen, width=10)
         self.languages_entry = tk.Entry(master=self.screen, width=10)
 
+        self.name_entry.grid(row=0, column=1, padx=1, pady=1)
+        self.hp_entry.grid(row=1, column=1, padx=1, pady=1)
+        self.speed_entry.grid(row=2, column=1, padx=1, pady=1)
+        self.cr_entry.grid(row=3, column=1, padx=1, pady=1)
+        self.type_entry.grid(row=4, column=1, padx=1, pady=1)
+        self.size_entry.grid(row=5, column=1, padx=1, pady=1)
+        self.alignment_entry.grid(row=6, column=1, padx=1, pady=1)
+        self.senses_entry.grid(row=7, column=1, padx=1, pady=1)
+        self.languages_entry.grid(row=8, column=1, padx=1, pady=1)
+
     def draw_buttons(self):
+        self.search_button = tk.Button(master=self.screen, text="Search", width=10, height=1, command=self.search)
+        self.help_button = tk.Button(master=self.screen, text="Help", width=10, height=1, command=self.draw_help)
+        
+        self.search_button.grid(row=0, column=2, padx=1, pady=1)
+        self.help_button.grid(row=8, column=2, padx=1, pady=1)
+
+    def search(self):
         pass
+
+    def draw_help(self):
+        helpscreen = help.HelpScreen("monster_lookup")
